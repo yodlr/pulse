98241c477300d03dc39b192a4de22300e5ccfa83
Ross Kukulinski <ross@kukulinski.com>
2015-03-23 16:36:44 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+var Pulse = module.exports = function pulse(metric) {
+  if (!(this instanceof Pulse)) {
+    return new Pulse(metric);
+  }
+  var p = this;
+  p._metric = metric;
+  p._measureLoop();
+}
+
+Pulse.prototype._measureLoop = function _measureLoop() {
+  var p = this;
+  blocked(function blockHandler(ms) {
+    p._metric.count('health.blocked', ms);
+  });
+
+  memory(function memHandler(heapUsed, rss) {
+    p._metric.gauge('health.heapUsed', heapUsed);
+    p._metric.gauge('health.rss', rss);
+    p._metric.gauge('health.uptime', process.uptime());
+  })
+}
+
+var blocked = function(fn) {
+  var start = process.hrtime()
+  var interval = 100;
+
+  setInterval(function(){
+    var delta = process.hrtime(start);
+    var nanosec = delta[0] * 1e9 + delta[1];
+    var ms = nanosec / 1e6;
+    var n = ms - interval;
+    if (n > 10) {
+      fn(Math.round(n))
+      console.log(Math.round(n));
+    }
+
+    //fn(Math.round(n))
+    start = process.hrtime();
+  }, interval).unref();
+};
+
+var memory = function(fn) {
+  var interval = 1000;
+
+  setInterval(function() {
+    var mem = process.memoryUsage();
+    fn(mem.heapUsed, mem.rss);
+  })
+}
